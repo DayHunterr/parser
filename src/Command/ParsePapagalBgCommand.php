9dae9bb16475e72001361d6953e66e6af9f49cfe
Dmytro <dmytro.vasiuk@gmail.com>
2024-03-15 12:02:46 +0200

    changed parser structure

--- src/Command/ParsePapagalBgCommand.php
+++ src/Command/ParsePapagalBgCommand.php
@@ -2,180 +2,187 @@
 
 namespace App\Command;
 
+use App\Util\CrawlerWrapper;
+use App\Util\TextUtil;
+use App\Util\Writer;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DomCrawler\Crawler;
 
 class ParsePapagalBgCommand extends Command
 {
+    private const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
+        'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'ю', 'я', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9'
+    ];
+
     private const BASE_URI = 'https://papagal.bg';
-    private const SEARCH_URI = 'https://papagal.bg/search_results/';
-    private const SEARCH_URI2 = '?type=company';
-    private const PAGE_URI3 = '&page=';
+    private const SEARCH_URI = self::BASE_URI.'/search_results/%s?type=company&page=%s';
+    private const RESULT_PATTERN = "%s}##{%s}##{%s}##{%s}##{%s}##{%s}##{%s}##{%s\n";
+    private const ERROR_PATTERN = "%s}##{%s\n";
+
     protected static $defaultName = 'parse:papagal_bg';
     protected static $defaultDescription = 'Parsing website papagal.bg';
-    protected function configure(): void
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct()
     {
+        parent::__construct();
 
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-
-        $client = new Client([
+        $this->client = new Client([
             'headers' => [
                 'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                 'Accept-Encoding' => 'gzip, deflate, br',
                 'Accept-Language' => 'en-US,en;q=0.5',
                 'Connection' => 'keep-alive',
                 'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'
-            ]
+            ],
         ]);
+    }
 
-        $charArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $resultWriter = new Writer('result/bg/papagal/result.csv');
+        $errorWriter = new Writer('result/bg/papagal/error.csv');
+        $profileErrorWriter = new Writer('result/bg/papagal/profile_error.csv');
 
-        $length = sizeof($charArray);
-
-        for ($keyWord = 0; $keyWord < $length; $keyWord++) {
-
-            $char = $charArray[$keyWord];
-
+        foreach (self::ALPHABET as $symbol) {
             for ($page = 1; $page < 30000; $page++) {
+                sleep(5);
+                $url = sprintf(self::SEARCH_URI, $symbol, $page);
 
-                $pageId = $page;
+                try {
+                    $response = $this->client->get($url);
 
-                $result = self::SEARCH_URI . $char . self::SEARCH_URI2 . self::PAGE_URI3 . $pageId;
-
-                dump($result);
-
-                $response = $client->get($result);
-
-                $statusCode = $response->getStatusCode();
-
-                if ($statusCode == '200') {
-
-                    $content = $response->getBody()->getContents();
-
-                    $crawler = new Crawler($content);
-
-                    if ($crawler->filter('table tbody tr')->count() === 0) {
-                        break;
+                    if ($response->getStatusCode() !== 200) {
+                        $errorWriter->write(sprintf(self::ERROR_PATTERN, $url, $response->getStatusCode()));
+                        continue;
                     }
 
-                    $filter = $crawler->filter('table tbody tr');
+                    $crawler = new CrawlerWrapper((string)$response->getBody());
 
-                    $data = $filter->each(function (Crawler $row) use ($client) {
+                    if (!$crawler->getTotalNodes('table tbody tr')) {
+                        continue 2;
+                    }
 
-                        $result = '';
-
-                        if ($row->filter('th')->count() === 0) {
+                    $rows = $crawler->filter('table tbody tr');
+                    $rows->each(function (CrawlerWrapper $row) use ($resultWriter, $profileErrorWriter) {
+                        if (!$profileRef = $row->getNodeAttr('th + td > a', 'href')) {
                             return;
                         }
 
-                        $nip = $row->filter('th')->text();
+                        $nameString = $row->getNodeHtml('th + td a');
+                        $address = $row->getNodeText('td +td + td');
 
-                        $name = $row->filter('th + td a')->html();
+                        $nip = TextUtil::clearString($row->getNodeText('th'));
+                        $name = $this->extractName($nameString);
+                        $originName = $this->extractOriginName($nameString);
+                        $date = $row->getNodeText('td + td');
+                        $postalCode = $this->extractPostalCode($address);
+                        $city = $this->extractCity($address);
 
-                        $name = preg_replace('/\n/', '', $name);
-                        $name = trim(preg_replace('/\s+/', ' ', $name));
-                        $name = str_replace('&amp;', '&', $name);
-                        $parts = explode('<br>', $name);
-                        $originName = trim($parts[0] ?? '');
-                        $name = trim($parts[1] ?? '');
+                        $profileUrl = self::BASE_URI . $profileRef;
+                        try {
+                            $profileResponse = $this->client->get($profileUrl);
 
-                        $result .= $originName . '}##{';
-                        $result .= $name . '}##{';
-                        $result .= $nip . '}##{';
-
-                        $rawDate = $row->filter('td + td')->text();
-                        $pattern = '/^(\w+) (\d{1,2}), (\d{4}) (\d{1,2}):(\d{2})$/';
-
-                        $converted_date = '';
-                        if (preg_match('/^\d-\d{4}-\d{2} \d{2}:\d{2}:\d{2}/', $rawDate)) {
-
-                            $timestamp = strtotime($rawDate);
-                            $converted_date = date('Y-m-d H:i:s', $timestamp);
-
-                        } elseif (preg_match('/^\d-\d{4}-\d{2} \d{2}:\d{2}/', $rawDate)) {
-
-                            $timestamp = strtotime($rawDate);
-                            $converted_date = date('Y-m-d H:i', $timestamp);
-
-                        } elseif (preg_match('/^\d-\d{4}-\d{2}/', $rawDate)) {
-
-                            $timestamp = strtotime($rawDate);
-                            $converted_date = date('Y-m-d', $timestamp);
-
-                        } elseif (preg_match('/^(\w+) (\d{1,2}), (\d{4}) (\d{1,2}):(\d{2})$/', $rawDate)) {
-                            $replacement = '${3}-${1}-${2} ${4}:${5}:${6}';
-                            $converted_date = preg_replace($pattern, $replacement, $rawDate);
-
-                        } elseif (preg_match('/\d{4}/', $rawDate)) {
-
-                            $timestamp = strtotime($rawDate);
-                            $converted_date = date('Y', $timestamp);
-
-                        }
-
-                        $postalCode = $row->filter('td + td + td')->text();
-                        $postalCode = preg_replace('/[^\d]+/', '', $postalCode);
-                        $postalCode = trim(preg_replace('/\s+/', ' ', $postalCode));
-
-                        $city = $row->filter('td + td + td')->text();
-                        $city = str_replace('БЪЛГАРИЯ,', '', $city);
-                        $city = trim(preg_replace('/\(\d+\)/', '', $city));
-
-                        $profileLink = self::BASE_URI . $row->filter('th + td > a')->attr('href');
-                        $profileResponse = $client->get($profileLink);
-                        $statusCodeProfile = $profileResponse->getStatusCode();
-                        if ($statusCodeProfile == '200') {
-                            $profileContent = $profileResponse->getBody()->getContents();
-
-                            $profileCrawler = new Crawler($profileContent);
-
-                            if ($profileCrawler->filterXPath('//dl[contains(.,"Статус")]')->count() !== 0) {
-                                $statusProfile = $profileCrawler->filterXPath('//dt[contains(.,"Статус")]//following-sibling::dd[1]')->text('UTF-8');
-                            } else {
-                                $statusProfile = '';
+                            if ($profileResponse->getStatusCode() !== 200) {
+                                $profileErrorWriter->write(sprintf(self::ERROR_PATTERN, $profileUrl));
+                                return;
                             }
 
-                            $result .= $statusProfile . '}##{';
-                            $result .= $converted_date . '}##{';
+                            $crawler = new CrawlerWrapper((string)$profileResponse->getBody());
+                            $profileAddress = $crawler->getXPathText('//dt[contains(.,"Седалище адрес")]//following-sibling::dd[1]');
 
-                            $street = '';
-                            if ($profileCrawler->filterXPath('//dl[contains(.,"Статус")]')->count() !== 0) {
-                                $address = $profileCrawler->filterXPath('//dt[contains(.,"Седалище адрес")]//following-sibling::dd[1]')->text();
-                                $street = str_replace(['БЪЛГАРИЯ,', $city . ',', $city], '', $address);
-                                $street = preg_replace('/(Виж на картата.+)|(Виж на картата)/', '', $street);
+                            $status = $crawler->getXPathText('//dt[contains(.,"Статус")]//following-sibling::dd[1]');
+                            $street = $this->extractStreet($profileAddress, $city);
 
-                            }
-
-                            $result .= trim($street . '}##{');
-                            $result .= $city . '}##{';
-                            $result .= $postalCode . '}##{' . PHP_EOL;
-
-                            sleep(1);
+                            $companyData = sprintf(self::RESULT_PATTERN, $originName, $name, $nip, $status, $date,
+                                $street, $city, $postalCode);
+                            $resultWriter->write($companyData);
+                        } catch (\Throwable $ex){
+                            $profileErrorWriter->write(
+                                sprintf(self::ERROR_PATTERN, $profileUrl, $ex->getMessage())
+                            );
                         }
-                        return $result;
                     });
-
-                    $fp = fopen('output.csv', 'a+');
-                    if ($fp) {
-                        foreach ($data as $row) {
-                            fputcsv($fp, [$row]);
-                        }
-                        fclose($fp);
-                        $io->success('CSV file has been created successfully.');
-                    } else {
-                        $io->error('Failed to open CSV file for writing.');
-                    }
+                } catch (\Throwable $ex){
+                    $errorWriter->write(sprintf(self::ERROR_PATTERN, $url, $ex->getMessage()));
                 }
             }
         }
+
         return Command::SUCCESS;
+    }
+
+    /**
+     * @param string|null $string
+     *
+     * @return string|null
+     */
+    private function extractOriginName(?string $string): ?string
+    {
+        $parts = explode('<br>', $string);
+
+        return TextUtil::clearString($parts[0] ?? null);
+    }
+
+    /**
+     * @param string|null $string
+     *
+     * @return string|null
+     */
+    private function extractName(?string $string): ?string
+    {
+        $parts = explode('<br>', $string);
+
+        return TextUtil::clearString($parts[1] ?? null);
+    }
+
+    /**
+     * @param string|null $address
+     *
+     * @return string|null
+     */
+    private function extractPostalCode(?string $address): ?string
+    {
+        preg_match('/\(\d{4}\)/', $address, $match);
+
+        return TextUtil::removeChars($match[0] ?? null);
+    }
+
+    /**
+     * @param string|null $address
+     *
+     * @return string|null
+     */
+    private function extractCity(?string $address): ?string
+    {
+        $city = preg_replace('/(\(.+\))|БЪЛГАРИЯ,/', '', $address);
+
+        return TextUtil::clearString($city);
+    }
+
+    /**
+     * @param string|null $profileAddress
+     * @param string|null $city
+     *
+     * @return string|null
+     */
+    private function extractStreet(?string $profileAddress, ?string $city): ?string
+    {
+        $cityAndComma = sprintf('%s,', $city);
+        $street = str_replace(['БЪЛГАРИЯ,', $cityAndComma, $city], '', $profileAddress);
+        $street = preg_replace('/(Виж на картата.+)|(Виж на картата)/', '', $street);
+
+        return TextUtil::clearString($street);
     }
 }
